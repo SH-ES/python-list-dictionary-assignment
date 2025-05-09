e0ffb2751831bb12fc96885845f167495c26dee0
SH-ES <shahina.hussains@gmail.com>
2025-05-10 00:48:48 +0500

    Add files via upload

---
+++ list_dictionary_practice.py
@@ -0,0 +1,19 @@
+#fruits= ["orange", "mango", "apricot", "pineapple"]
+#print("Fruit Names:", fruits)
+#print("Second Fruit Name:", fruits[1])
+#fruits.append("chickoo")
+#print("updated fruit list", fruits)
+
+#Dictionary
+
+student = {
+    "name":"Shahina",
+    "age":51,
+    "city":"Karachi"
+}
+#print("Student information:", student)
+#print(student(0))
+#print("Name:", student["name"])
+student["School"]="Allama Iqbal School"
+print("Updated Student Information:",student)
+#I don't know hoe to push file. So I am uploading to Git
